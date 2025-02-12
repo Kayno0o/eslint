6b1fa507fe97bbb085376da214f2372c0a31a8a2
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-02-12 08:58:58 +0100

    first commit

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  clean: true,
+  entry: ['./index.js'],
+  format: ['cjs', 'esm'],
+  minify: true,
+  sourcemap: true,
+  splitting: false,
+  external: ['dayjs', 'ansi-colors'],
+})
