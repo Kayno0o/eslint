98b6aeeb5f76217f0b95b5d32def125a9d69da5d
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-09-30 10:53:13 +0200

    feat(Style): padding-line-between-statements

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import type { Rules } from '@antfu/eslint-config'
 import type { Linter } from 'eslint'
 
+type StatementType = '*' | 'exports' | 'require' | 'directive' | 'iife' | 'block' | 'empty' | 'function' | 'ts-method' | 'break' | 'case' | 'class' | 'continue' | 'debugger' | 'default' | 'do' | 'for' | 'if' | 'import' | 'switch' | 'throw' | 'try' | 'while' | 'with' | 'cjs-export' | 'cjs-import' | 'enum' | 'interface' | 'function-overload' | 'block-like' | 'singleline-block-like' | 'multiline-block-like' | 'expression' | 'singleline-expression' | 'multiline-expression' | 'return' | 'singleline-return' | 'multiline-return' | 'export' | 'singleline-export' | 'multiline-export' | 'var' | 'singleline-var' | 'multiline-var' | 'let' | 'singleline-let' | 'multiline-let' | 'const' | 'singleline-const' | 'multiline-const' | 'using' | 'singleline-using' | 'multiline-using' | 'type' | 'singleline-type' | 'multiline-type'
+type StatementArrayType = [StatementType, ...StatementType[]]
+
+const paddingBefore: StatementArrayType = ['return', 'try', 'case', 'continue', 'break']
+const paddingAfter: StatementArrayType = [] as any
+const paddingAround: StatementArrayType = ['if', 'function', 'switch', 'while']
+
+const paddingGroups: StatementArrayType[] = [
+  ['const', 'let'],
+]
+
 export const commonRules: Partial<Linter.RulesRecord & Rules> = {
   'antfu/no-top-level-await': 'off',
   'github/array-foreach': 'error',
@@ -16,7 +27,6 @@ export const commonRules: Partial<Linter.RulesRecord & Rules> = {
   'operator-assignment': ['error', 'always'],
   'style/array-bracket-newline': ['warn', 'consistent'],
   'style/array-element-newline': ['warn', 'consistent'],
-  'style/function-paren-newline': ['warn', { minItems: 5 }],
   'style/object-curly-newline': ['warn', { consistent: true }],
   'style/wrap-regex': 'warn',
   'ts/adjacent-overload-signatures': 'error',
@@ -37,6 +47,18 @@ export const commonRules: Partial<Linter.RulesRecord & Rules> = {
   'unicorn/no-new-array': 'off',
   'unused-imports/no-unused-vars': ['error', { argsIgnorePattern: '^_', caughtErrors: 'none' }],
   'style/indent': ['error', 2, { CallExpression: { arguments: 'first' } }],
+
+  'style/padding-line-between-statements': [
+    'error',
+
+    ...paddingGroups.flatMap(block => [
+      { blankLine: 'always' as const, prev: block, next: '*' as const },
+      { blankLine: 'any' as const, prev: block, next: block },
+    ]),
+
+    { blankLine: 'always', prev: '*', next: [...paddingAround, ...paddingBefore] },
+    { blankLine: 'always', prev: [...paddingAround, ...paddingAfter], next: '*' },
+  ],
 }
 
 export * from './astro'
