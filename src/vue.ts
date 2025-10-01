6a882d01ee74ecb53dda485688e007fe1273518f
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-10-01 10:46:23 +0200

    feat: improve vue rules

--- src/vue.ts
+++ src/vue.ts
@@ -1,43 +1,76 @@
-import type { Awaitable, OptionsConfig, OptionsFormatters, TypedFlatConfigItem } from '@antfu/eslint-config'
+import type { Awaitable, OptionsConfig, TypedFlatConfigItem } from '@antfu/eslint-config'
 import type { FlatConfigComposer } from 'eslint-flat-config-utils'
 import type { Linter } from 'eslint/universal'
 import antfu from '@antfu/eslint-config'
-import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
+import eslintPluginTailwindCSS from 'eslint-plugin-better-tailwindcss'
 import github from 'eslint-plugin-github'
 import _ from 'lodash'
 import { commonRules } from '~'
 
-export function vue(options?: {
+export function vue({
+  i18n = false,
+  tailwindcss = undefined,
+  tailwindcssConfigFile = 'tailwind.config.ts',
+  tailwindcssEntryPoint = 'app/assets/css/main.css',
+  mergeOptions = {},
+  tailwindcssMultiline = false,
+}: {
   i18n?: boolean
-  formatters?: OptionsFormatters
+  tailwindcss?: 3 | 4
+  tailwindcssConfigFile?: string
+  tailwindcssEntryPoint?: string
   mergeOptions?: OptionsConfig & Omit<TypedFlatConfigItem, 'files'>
+  tailwindcssMultiline?: boolean
 }, ...userConfigs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[] | FlatConfigComposer<any, any> | Linter.Config[]>[]) {
   return antfu(_.merge({
-    plugins: { github, 'better-tailwindcss': eslintPluginBetterTailwindcss },
+    plugins: {
+      github,
+      ...(tailwindcss && { 'better-tailwindcss': eslintPluginTailwindCSS }),
+    },
     typescript: true,
-    //
     vue: { a11y: true, vueVersion: 3 },
-    formatters: { css: true, ...(options?.formatters ?? {}) },
-    rules: _.merge(commonRules, {
+    formatters: { css: true },
+    rules: {
+      ...commonRules,
       'vue/max-attributes-per-line': ['error', { multiline: { max: 1 }, singleline: { max: 3 } }],
       'vue/html-button-has-type': 'error',
       'vue/array-bracket-newline': ['warn', 'consistent'],
       'vue/array-element-newline': ['warn', 'consistent'],
       'vue/object-curly-newline': ['warn', { consistent: true, multiline: true }],
       'vue/no-implicit-coercion': ['error', { number: true, boolean: true, string: true, disallowTemplateShorthand: true }],
-      'vue/static-class-names-order': 'error',
-      ...(options?.i18n && {
-        'vue/no-bare-strings-in-template': 'error',
-      }),
+      'vue/require-explicit-emits': 'error',
+      'vue/require-explicit-slots': 'warn',
       // false positive
       'vue-a11y/label-has-for': 'off',
-      // tailwindcss 4
-      ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
-      'better-tailwindcss/no-unregistered-classes': ['warn', { detectComponentClasses: true }],
-      'better-tailwindcss/enforce-consistent-line-wrapping': ['warn', { group: 'newLine', printWidth: 120 }],
-      // : ['warn', { printWidth: 0, preferSingleLine: true }],
-    }),
-  }, options?.mergeOptions), ...userConfigs)
+      // custom options
+      ...(i18n && {
+        'vue/no-bare-strings-in-template': ['error'],
+      }),
+      ...(tailwindcss && {
+        ...eslintPluginTailwindCSS.configs['recommended-warn'].rules,
+        'better-tailwindcss/no-unregistered-classes': ['warn', { detectComponentClasses: true }],
+        'better-tailwindcss/enforce-consistent-line-wrapping': tailwindcssMultiline
+          ? ['warn', { group: 'newLine', printWidth: 120 }]
+          : ['warn', { printWidth: 0, preferSingleLine: true }],
+      }),
+      ...mergeOptions?.rules,
+    },
+  }, tailwindcss === 3 && {
+    rules: {
+      'better-tailwindcss/no-conflicting-classes': 'off',
+    },
+    settings: {
+      'better-tailwindcss': {
+        tailwindConfig: tailwindcssConfigFile ?? 'tailwind.config.ts',
+      },
+    },
+  }, tailwindcss === 4 && {
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: tailwindcssEntryPoint ?? 'app/assets/css/main.css',
+      },
+    },
+  }, mergeOptions), ...userConfigs)
 }
 
 export default vue
